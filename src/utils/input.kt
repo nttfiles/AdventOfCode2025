573d538863b0bf8f3c7d402cbf3c3ff0e99e8dd9
Pixelguymm <maartenmarx@hotmail.be>
2025-12-07 16:24:31 +0100

    Remove unused lines() function

--- src/utils/input.kt
+++ src/utils/input.kt
@@ -3,6 +3,5 @@ package utils
 fun resource(name: String) = {}.javaClass.getResource("/$name")?.readText()?.trim()
     ?: error("No resource named \"$name\"")
 
-fun String.lines() = this.split(Regex("[\n\r]+"))
 fun String.words() = this.split(Regex("\\s+")).filter { it.isNotEmpty() }
 fun String.splitByWhitespace() = this.split(Regex("([\n\r]+)\\1+"))
